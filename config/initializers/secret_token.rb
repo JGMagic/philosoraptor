c2c37f493888e9053f0aaf5058a69ef2f710325d
Joel Goldman <jgmagic@gmail.com>
2012-09-06 10:53:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Philosoraptor::Application.config.secret_token = 'c99e629427e0f3115144e6e2e73bd2bd63016c987fad732e14517d4a78e20c0d27003dd18ed2eb0694f5ae4ace7cba6ebab1ee2d9bc2b315ae975f878adf31ae'
